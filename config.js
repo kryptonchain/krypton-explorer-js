0cd3bf8ae22a204be5303bf0861c59cb3341ebd6
root <root@vultr.guest>
2019-09-22 22:56:29 +0000

    fix typo in calculation

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var api = 'https://explorer.kryptonchain.org/node-api';
 var donationAddress = "";
 var blockTargetInterval = 60; // enter the block interval in seconds
 var coinUnits = 1000000;  // enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 trtl
-var totalSupply = 100000000000000; // enter the total supply in atomic units
+var totalSupply = 88000000000000; // enter the total supply in atomic units
 var symbol = 'ZOD'; // enter the coin's ticker
 var refreshDelay = 30000;
 
